4f619a14f431db6bdf2844cb2446f96dc93e6b95
lauroca <senaicurso@gmail.com>
2026-09-15 11:39:12 -0300

    adicionada pasta 15.09 - variaveis

---
+++ 15.09/script.js
@@ -0,0 +1,32 @@
+let zendaya = "mary jane"
+let zedaya = 6767676767;
+let numero = "6767676767";
+// let idade = 17;
+
+
+// idade = 20;
+//  idade = 21;
+
+
+// console.log("a mary jane é a " + zendaya + "e ela tem " + idade + " anos");
+let nome = "Laura Ramos";
+let sobrenome = " ramos "
+let idade = "15 anos"
+
+let curso = "curso de java"
+let peso = 89.5;//int
+let altura = 1.80;//float
+let funcionario = true;//bool true = verdadeiro ou false
+// console.log("o meu nome é " + nome + " eu tenho " + idade + " estou cursando o " + curso + "tenho cerca de"+ peso + "kg")
+
+console.log("a variavel " + nome +"tem um tipo que é " + typeof nome)
+console.log("a variavel " + sobrenome + "tem um tipo que é " + typeof sobrenome)
+console.log("a variavel " + peso + "tem um tipo que é " + typeof peso)
+console.log("a variavel " + altura + "tem um tipo que é " + typeof altura)
+console.log("a variavel " + funcionario + "tem um tipo que é " + typeof funcionario)
+
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionario);
+
