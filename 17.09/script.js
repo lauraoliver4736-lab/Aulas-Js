14d2fd31cab47b473f3eacc4d69c0ee31f293941
lauroca <senaicurso@gmail.com>
2026-09-17 11:57:55 -0300

    exercicios aula 17 finalizados

---
+++ 17.09/script.js
@@ -0,0 +1,122 @@
+//let nota= 6;
+
+//if (nota < 5 ) {
+  //  console.log(" Henrique reprovado");
+    
+// else if (nota < 7) {
+// console.log(" Henrique reprovado");
+ 
+    
+// else{
+//console.log("henrique aprovado");
+
+
+
+
+let temperatura = 17
+//Exercicio 1 
+if (temperatura > 30) {
+    console.log("Esta quente");
+
+    }
+    else if (temperatura >20 && temperatura < 30) {
+        console.log("Temperatura agradavel");
+     }   
+    else  {
+        console.log("Esta frio");
+        
+    }
+    //Exercicio 2
+    let velocidade = 43858
+if (velocidade<= 60) {
+    console.log("velocidade permitida");
+    
+}
+else if (velocidade >60 && velocidade < 80) {
+    console.log("atenção: velocidade acima do permitido");
+    
+} else {
+    console.log("velocidade muito alta");
+    
+}
+
+//exercicio 3 
+let QuantidadeEstoque= 2938
+
+if (QuantidadeEstoque == 0) {
+    console.log("produto esgotado");
+    
+}
+if (QuantidadeEstoque >1 && QuantidadeEstoque < 10) {
+    console.log("estoque baixo");
+    
+} else {
+    console.log("estoque disponivel");
+    
+}
+
+// exercicio 4
+let user= "maquiavel"
+let senha = "6767"
+
+
+    
+if (user == "admin" && senha == "1234") {
+    console.log("login correto");
+} else {
+    console.log("Usuario e senha incorretos");
+    
+}
+
+//exercicio 5
+let hora = 22
+if (hora > 0 && hora<11) {
+    console.log("Bom Dia")
+    
+}else if (hora > 12 && hora < 17) {
+    console.log("boa tarde")
+    
+}else if (hora > 18 && hora < 23) {
+    console.log("boa noite")
+    
+}
+//exercicio 6
+let nota = 46
+if (nota == 5) {
+  console.log("excelente");
+    
+}
+else if (nota == 4) {
+    console.log(" Muito bom");
+    
+}
+else if (nota == 3) {
+    console.log("Bom");
+    
+}
+else if (nota == 2) {
+    console.log("regular");
+    
+}
+if (nota == 1  ) {
+      console.log("ruim")
+} else {
+console.log("Nota invalida");
+
+ }  
+ //exercicio 07 
+ let TipoIngresso= "inteira"
+
+if (TipoIngresso == "meia") {
+    console.log("R$ 20");
+}
+else if (TipoIngresso == "inteira") {
+    console.log("R$ 40");
+}
+else if (TipoIngresso == "Vip") {
+    console.log("R$ 80");
+    
+} else {
+    console.log("tipo de ingresso invalido");
+    
+}
