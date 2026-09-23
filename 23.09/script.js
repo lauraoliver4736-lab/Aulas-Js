d561b580b8ecb99451ce2175997c014c6411c91b
lauroca <senaicurso@gmail.com>
2026-09-23 11:54:08 -0300

    aula 23.09 - estrutura de repetição

---
+++ 23.09/script.js
@@ -0,0 +1,138 @@
+// let idade= 21
+
+// if (idade >= 18) {
+//     console.log("maior de idade")
+// } else {
+//     console.log("menor de idade")
+// }
+
+// let media = 7
+// if (media >= 6) {
+//     console.log("Aprovado")
+// } else if {(media == 5 || media == 6)
+//     console.log("Recuperação")
+// 
+// else 
+// console.log("1")
+// console.log("2")
+// console.log("3")
+// console.log("4")
+// console.log("5")
+// console.log("6")
+// console.log("7")
+// console.log("8")
+// console.log("9")
+// console.log("10")
+
+//for
+// for (variavel; condicao; incremento){
+
+// }
+// incremento
+// for (let index = 0; index < 10; index++) {
+// console.log(index);
+// }
+
+// //recremento
+// for (let index = 10; index >= 0; index--) {
+// console.log(index);
+// }
+// // Exercicio 1
+// for (let index = 1; index < 20; index++) {
+//     console.log(index);
+     
+// }
+// // Exercicio 2
+// for (let index = 5; index <= 50; index += 5) {
+//     console.log(index);
+// }
+// //Exercicio 3
+// for (let index = 10; index >= 0; index --) {
+// console.log(index);
+// }
+// console.log("contagem finalizada");
+// While
+// let contador=0;
+// while (contador <= 10) {
+    
+//  console.log(contador);
+ 
+//  contador +=5;
+
+// }
+
+//exercicio 1
+let number = 10;
+
+while (number <= 20) {
+    
+    console.log(number);
+    
+
+    number ++;
+}
+//exercicio 2
+let numero = 2;
+while (numero <= 20) {
+    console.log(numero);
+    
+
+    numero +=2;
+}
+
+let n= 10;
+while (n<5) {
+    console.log("WHILE");
+    
+
+}
+
+do {
+    console.log("DO...WHILE");
+    
+} while (n<5);
+
+let contador = 0;
+do {
+    console.log(contador);
+    
+    contador ++;
+
+} while (contador <=10);
+
+// Exercicio 1
+let  numeros = 10;
+do {
+    console.log(numeros);
+
+    numeros ++;
+
+} while (numeros <=100);
+
+
+
+//Exercicio 2
+let contagem = 10
+do {
+    console.log(contagem);
+    
+    contagem --;
+
+} while (contagem >= 1);
+
+let nome= "levi"
+let nome2= "Duda"
+let nome3= "Gustavo"
+let nome4= "Bernardo"
+
+// 
+let nomes = ["levi","Duda","Gustavo","Bernardo"];
+
+console.log(nomes);//mostra a lista completa na mesma linha 
+
+console.log(nomes[1]);//mostra o item da posição mencionada entre colchetes
+
+nome4= "Ana";
+nomes[3]= "Ana"//Altero o valor da posição especificada
+
+console.log(nomes.length );
